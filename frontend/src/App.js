8487a3830dd1b9bcb78fee78d07cdb14d0578e59
Roger Hall <rogerhall123@gmail.com>
2020-12-16 13:04:41 -0700

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+
+import React, { Fragment } from 'react';
+
+
+const App = () => {
+  
+    return (
+
+        <Fragment>
+
+            <h1>Welcome to the ProShop</h1>
+
+        </Fragment>
+
+    );
+
+}
+
+
+export default App;
